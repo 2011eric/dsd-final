f466780f7fcf20933686de8db7050b5b08b08ab9
Hans Gao <gaohengsiang@gmail.com>
2024-06-12 00:35:43 +0800

    create files

--- Src/branch_predictor.v
+++ Src/branch_predictor.v
@@ -1,14 +1,16 @@
 module branch_predictor(
     input clk,
     input rst_n,
-    input [31:0] branch_pc, // BHB will use branch_pc to index the saturation counter
+    input [31:0] branch_pc, // BTB will use branch_pc to index the saturation counter
     input prediction_correct,
+    input [31: 0] correct_destination,
     input feedback_valid,//if the instruction in EX is not a branch, or if it is stalling, 
-        //the saturation counter should not take this feedback
-    output take_branch
+    //the saturation counter should not take this feedback
+    output take_branch,
+    output [31: 0] predicted_destination
 );
 
-`ifndef USE_BHB
+`ifndef USE_BTB
     saturation_counter u_saturation_counter (
         .clk                   (clk),
         .rst_n                 (rst_n),
@@ -16,9 +18,19 @@ module branch_predictor(
         .feedback_valid        (feedback_valid),
         .take_branch           (take_branch)
     );
+    assign predicted_destination = 0;
 `else
-    parameter BHB_SIZE = 4;
+    parameter BTB_SIZE = 4;
+    BHT u_bhb (
+        .clk                    (clk),
+        .rst_n                  (rst_n),
+        .branch_pc_i            (branch_pc),
+        .prediction_correct_i   (prediction_correct),
+        .feedback_valid_i       (feedback_valid),
+        .take_branch_o          (take_branch)
+    );
+    BTB u_btb (
+
+    );
 `endif
-
-
 endmodule
